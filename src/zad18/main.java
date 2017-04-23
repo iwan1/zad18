7636f1d6b77140863b324a93fb87ce0420d41378
iwan <iwan@iwan-PC>
2017-04-23 19:56:02 +0200

    avb

--- src/zad18/main.java
+++ src/zad18/main.java
@@ -6,9 +6,18 @@ public class main {
 		// TODO Auto-generated method stub
 		String[] napis = new String[5];
 		try{
-		System.out.println(napis[1].length());
+			System.out.println(napis[1].length());
 		}catch(NullPointerException e){
 			System.out.println("Odowa³anie siê do niezainicjowanej zmiennej!");
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Odwo³anie siê poza zakres tablicy!");
+		}
+		try{
+			System.out.println(napis[7].length());
+		}catch(NullPointerException e){
+			System.out.println("Odowa³anie siê do niezainicjowanej zmiennej!");
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Odwo³anie siê poza zakres tablicy!");
 		}
 	}
 
